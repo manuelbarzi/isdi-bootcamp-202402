299c098f6bc488344d5c474c9c08034a4ef1b7a7
Manuel Barzi <manuelbarzi@gmail.com>
2024-03-13 10:32:17 +0100

    separate rendering in app into functions #318

---
+++ staff/manuel-barzi/isdigram/app.14/home/index.js
@@ -0,0 +1,281 @@
+// presentation
+
+(function () {
+    if (!logic.isUserLoggedIn()) {
+        location.href = '../login'
+
+        return
+    }
+
+    var title = document.querySelector('h1')
+    var logoutButton = document.querySelector('#logout-button')
+    var createPostSection = document.querySelector('#create-post-section')
+    var createPostForm = createPostSection.querySelector('form')
+    var createPostCancelButton = createPostSection.querySelector('#create-post-cancel-button')
+    var createPostButton = document.querySelector('#create-post-button')
+    var postListSection = document.querySelector('#post-list-section')
+    var chatButton = document.querySelector('#chat-button')
+    var chatSection = document.querySelector('#chat-section')
+    var chatPanel = chatSection.querySelector('#chat-panel')
+    var chatForm = chatPanel.querySelector('form')
+    var footer = document.querySelector('#footer')
+    var homeButton = document.querySelector('#home-button')
+    var editPostSection = document.querySelector('#edit-post-section')
+    var editPostCancelButton = editPostSection.querySelector('#edit-post-cancel-button')
+    var editPostForm = editPostSection.querySelector('form')
+
+    try {
+        var user = logic.retrieveUser()
+
+        title.innerText = 'Hello, ' + user.name + '!'
+    } catch (error) {
+        console.error(error)
+
+        alert(error.message)
+
+        try {
+            logic.logoutUser()
+
+        } catch (error) {
+            logic.cleanUpLoggedInUserId()
+        }
+
+        location.href = '../login'
+    }
+
+    logoutButton.onclick = function () {
+        logic.logoutUser()
+
+        location.href = '../login'
+    }
+
+    createPostForm.onsubmit = function (event) {
+        event.preventDefault()
+
+        var imageInput = createPostForm.querySelector('#image')
+        var image = imageInput.value
+
+        var textInput = createPostForm.querySelector('#text')
+        var text = textInput.value
+
+        try {
+            logic.createPost(image, text)
+
+            createPostForm.reset()
+
+            createPostSection.style.display = 'none'
+
+            renderPosts()
+        } catch (error) {
+            console.error(error)
+
+            alert(error.message)
+        }
+    }
+
+    createPostButton.onclick = function () {
+        createPostSection.style.display = 'block'
+    }
+
+    createPostCancelButton.onclick = function () {
+        createPostSection.style.display = 'none'
+    }
+
+    function renderPosts() {
+        try {
+            var posts = logic.retrievePosts()
+
+            postListSection.innerHTML = ''
+
+            posts.forEach(function (post) {
+                var article = document.createElement('article')
+
+                var authorHeading = document.createElement('h3')
+                authorHeading.innerText = post.author.username
+
+                var image = document.createElement('img')
+                image.src = post.image
+
+                var paragraph = document.createElement('p')
+                paragraph.innerText = post.text
+
+                var dateTime = document.createElement('time')
+                dateTime.innerText = post.date
+
+                article.append(authorHeading, image, paragraph, dateTime)
+
+                if (post.author.id === logic.getLoggedInUserId()) {
+                    var deleteButton = document.createElement('button')
+
+                    deleteButton.innerText = '🗑️'
+
+                    deleteButton.onclick = function () {
+                        if (confirm('delete post?'))
+                            try {
+                                logic.removePost(post.id)
+
+                                renderPosts()
+                            } catch (error) {
+                                console.error(error)
+
+                                alert(error.message)
+                            }
+                    }
+
+                    var editButton = document.createElement('button')
+
+                    editButton.innerText = '📝'
+
+                    editButton.onclick = function () {
+                        var textInput = editPostForm.querySelector('#text')
+
+                        textInput.value = post.text
+
+                        editPostForm.onsubmit = function (event) {
+                            event.preventDefault()
+
+                            var text = textInput.value
+
+                            try {
+                                logic.modifyPost(post.id, text)
+
+                                editPostForm.reset()
+
+                                editPostSection.style.display = ''
+
+                                renderPosts()
+                            } catch (error) {
+                                console.error(error)
+
+                                alert(error.message)
+                            }
+                        }
+
+                        editPostSection.style.display = 'block'
+                    }
+
+                    article.append(deleteButton, editButton)
+                }
+
+                postListSection.appendChild(article)
+            })
+        } catch (error) {
+            console.error(error)
+
+            alert(error.message)
+        }
+    }
+
+    renderPosts()
+
+    var renderMessagesIntervalId
+
+    chatButton.onclick = function () {
+        postListSection.style.display = 'none'
+        footer.style.display = 'none'
+        chatButton.style.display = 'none'
+
+        homeButton.style.display = 'block'
+        chatSection.style.display = 'block'
+
+        var userList = chatSection.querySelector('#user-list')
+
+        userList.innerHTML = ''
+
+        try {
+            var users = logic.retrieveUsersWithStatus()
+
+            users.forEach(function (user) {
+                var item = document.createElement('li')
+
+                item.classList.add('user-list__item')
+
+                if (user.status === 'online')
+                    item.classList.add('user-list__item--online')
+                else if (user.status === 'offline')
+                    item.classList.add('user-list__item--offline')
+
+                item.innerText = user.username
+
+                item.onclick = function () {
+                    var usernameTitle = chatPanel.querySelector('#chat-panel__username')
+
+                    usernameTitle.innerText = user.username
+
+                    function renderMessages() {
+                        try {
+                            var messages = logic.retrieveMessagesWithUser(user.id)
+
+                            var messageList = chatPanel.querySelector('#message-list')
+
+                            messageList.innerHTML = ''
+
+                            messages.forEach(function (message) {
+                                var messageParagraph = document.createElement('p')
+
+                                messageParagraph.innerText = message.text
+
+                                if (message.from === logic.getLoggedInUserId())
+                                    messageParagraph.classList.add('message-list__item--right')
+                                else
+                                    messageParagraph.classList.add('message-list__item--left')
+
+                                messageList.appendChild(messageParagraph)
+                            })
+                        } catch (error) {
+                            console.error(error)
+
+                            alert(error.message)
+                        }
+                    }
+
+                    renderMessages()
+
+                    clearInterval(renderMessagesIntervalId)
+
+                    renderMessagesIntervalId = setInterval(renderMessages, 1000)
+
+                    chatForm.onsubmit = function (event) {
+                        event.preventDefault()
+
+                        var textInput = chatForm.querySelector('#text')
+                        var text = textInput.value
+
+                        try {
+                            logic.sendMessageToUser(user.id, text)
+
+                            chatForm.reset()
+
+                            renderMessages()
+                        } catch (error) {
+                            console.error(error)
+
+                            alert(error.message)
+                        }
+                    }
+
+                    chatPanel.style.display = 'block'
+                }
+
+                userList.appendChild(item)
+            })
+        } catch (error) {
+            console.error(error)
+
+            alert(error.message)
+        }
+    }
+
+    homeButton.onclick = function () {
+        homeButton.style.display = 'none'
+        chatSection.style.display = 'none'
+
+        postListSection.style.display = ''
+        footer.style.display = ''
+        chatButton.style.display = ''
+    }
+
+    editPostCancelButton.onclick = function () {
+        editPostSection.style.display = ''
+    }
+})()
---
+++ staff/manuel-barzi/isdigram/app.15/data/Collection.spec.js
@@ -0,0 +1,189 @@
+describe('Collection', function () {
+    describe('constructor', function () {
+        it('creates a collection', function () {
+            var cars = new Collection('cars')
+
+            expect(cars).toBeInstanceOf(Collection)
+        })
+    })
+
+    describe('> helpers', function () {
+        describe('_generateId', function () {
+            it('generates a random id', function () {
+                var cars = new Collection('cars')
+
+                var id1 = cars._generateId()
+
+                expect(typeof id1).toBe('string')
+
+                var id2 = cars._generateId()
+
+                expect(typeof id2).toBe('string')
+
+                expect(id1 === id2).toBe(false)
+            })
+        })
+
+        describe('_loadDocuments', function () {
+            it('loads empty array on new collection', function () {
+                delete localStorage.cars
+
+                var cars = new Collection('cars')
+
+                var documents = cars._loadDocuments()
+
+                expect(documents).toBeInstanceOf(Array)
+                expect(documents.length).toBe(0)
+            })
+
+            it('loads data on non-empty collection', function () {
+                localStorage.cars = '[{"brand":"porsche","model":"911"},{"brand":"fiat","model":"500"}]'
+
+                var cars = new Collection('cars')
+
+                var documents = cars._loadDocuments()
+
+                expect(documents).toBeInstanceOf(Array)
+                expect(documents.length).toBe(2)
+
+                var document = documents[0]
+                expect(document).toBeInstanceOf(Object)
+                expect(document.brand).toBe('porsche')
+                expect(document.model).toBe('911')
+
+                var document = documents[1]
+                expect(document.brand).toBe('fiat')
+                expect(document.model).toBe('500')
+            })
+        })
+
+        describe('_saveDocuments', function () {
+            it('saves a collection', function () {
+                delete localStorage.cars
+
+                var documents = [{ brand: 'porsche', model: '911' }, { brand: 'fiat', model: '500' }]
+
+                var cars = new Collection('cars')
+
+                cars._saveDocuments(documents)
+
+                //expect(!!localStorage.cars).toBe(true)
+                expect(typeof localStorage.cars).toBe('string')
+
+                var documentsJSON = '[{"brand":"porsche","model":"911"},{"brand":"fiat","model":"500"}]'
+
+                expect(localStorage.cars).toBe(documentsJSON)
+            })
+
+            it('fails on non-array documents', function () {
+                var documents = 'hola documents'
+
+                var cars = new Collection('cars')
+
+                var errorThrown
+
+                try {
+                    cars._saveDocuments(documents)
+                } catch (error) {
+                    errorThrown = error
+                }
+
+                expect(errorThrown).toBeInstanceOf(TypeError)
+                expect(errorThrown.message).toBe('documents is not an array')
+            })
+
+            it('fails on array with non-object document in documents', function () {
+                var documents = [{ brand: 'porsche', model: '911' }, { brand: 'fiat', model: '500' }, 'hola document']
+
+                var cars = new Collection('cars')
+
+                var errorThrown
+
+                try {
+                    cars._saveDocuments(documents)
+                } catch (error) {
+                    errorThrown = error
+                }
+
+                expect(errorThrown).toBeInstanceOf(TypeError)
+                expect(errorThrown.message).toBe('a document in documents is not an object')
+            })
+        })
+
+        describe('_backup', function () {
+            it('backs up the collection json', function () {
+                delete localStorage['cars-backup']
+
+                localStorage.cars = '[{"brand":"porsche","model":"911"},{"brand":"fiat","model":"500"}]'
+
+                var cars = new Collection('cars')
+
+                cars._backup()
+
+                expect(localStorage['cars-backup']).toBe(localStorage.cars)
+            })
+        })
+
+        describe('_restore', function () {
+            it('restores the collection json', function () {
+                delete localStorage['cars']
+
+                localStorage['cars-backup'] = '[{"brand":"porsche","model":"911"},{"brand":"fiat","model":"500"}]'
+
+                var cars = new Collection('cars')
+
+                cars._restore()
+
+                expect(localStorage.cars).toBe(localStorage['cars-backup'])
+            })
+        })
+    })
+
+    describe('> CRUD', function () {
+        describe('findOne', function () {
+            it('should find an existing document', function () {
+                localStorage.cars = '[{"brand":"porsche","model":"911"},{"brand":"fiat","model":"500"}]'
+
+                var cars = new Collection('cars')
+
+                var car = cars.findOne(function (car) { return car.brand === 'fiat' })
+
+                expect(car).toBeInstanceOf(Object)
+                expect(car.brand).toBe('fiat')
+                expect(car.model).toBe('500')
+            })
+
+            it('should fail on no callback', function () {
+                var cars = new Collection('cars')
+
+                var errorThrown
+
+                try {
+                    cars.findOne()
+                } catch (error) {
+                    errorThrown = error
+                }
+
+                expect(errorThrown).toBeInstanceOf(TypeError)
+                expect(errorThrown.message).toBe('callback is not a function')
+            })
+
+            it('should fail on non-function callback', function () {
+                var cars = new Collection('cars')
+
+                var errorThrown
+
+                try {
+                    cars.findOne(123)
+                } catch (error) {
+                    errorThrown = error
+                }
+
+                expect(errorThrown).toBeInstanceOf(TypeError)
+                expect(errorThrown.message).toBe('callback is not a function')
+            })
+        })
+
+        // TODO test all methods
+    })
+})
