dd5b0f8ea7e49e55afec8f02cb167319023581ce
Manuel Barzi <manuelbarzi@gmail.com>
2024-03-26 13:07:57 +0100

    add write file to create user in api #318

--- staff/manuel-barzi/isdigram/api/index.js
+++ staff/manuel-barzi/isdigram/api/index.js
@@ -10,9 +10,32 @@ try {
             return
         }
 
-        console.log(usersJson)
+        const users = JSON.parse(usersJson)
 
-        console.log('end')
+        const user = {
+            name: 'As Pirina',
+            birthdate: '2004-01-01',
+            email: 'as@pirina.com',
+            username: 'aspirina',
+            password: '123qwe123',
+            status: 'offline',
+            id: '3gfscipdb0c'
+        }
+
+        users.push(user)
+
+        const usersJson2 = JSON.stringify(users)
+
+        fs.writeFile('./users.json', usersJson2, error => {
+            if (error) {
+                console.error(error)
+
+                return
+            }
+
+            console.log('end')
+        })
+
     })
 } catch (error) {
     console.error(error)
